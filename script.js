027a65ee97b23c04edcd7113f1758e833736f20a
Basirat Hamayoon <basirathumayun@gmail.com>
2024-07-06 18:27:56 +0530

    Problem 02 solved

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 //Problem no 01
-var userInput = +prompt("Please enter a Number?")
-for (var i = 0; i < 10; i++)
-    console.log(`${userInput} * ${i} = ${userInput * i}`)
+//var userInput = +prompt("Please enter a Number?")
+//for (var i = 0; i < 10; i++)
+    //console.log(`${userInput} * ${i} = ${userInput * i}`)
 //Problem n0 02
+//var mainArray = [];
+//var sum = 0;
+//for (var i = 0; i < 5; i++){
+    //var userInput = +prompt("Please enter Elements to the Array")
+    //mainArray.push(userInput)
+//}
+//for (let i = 0; i < mainArray.length; i++){
+  //  sum += mainArray[i];
+//}
+
+
+//console.log(`The summ of the Array elements are: ${sum}`);
