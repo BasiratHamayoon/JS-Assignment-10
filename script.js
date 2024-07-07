bc29a257f16013508ac42bf729e77c9da6a399a7
Basirat Hamayoon <basirathumayun@gmail.com>
2024-07-07 12:41:49 +0530

    Assignment completed

--- script.js
+++ script.js
@@ -15,3 +15,34 @@
 
 
 //console.log(`The summ of the Array elements are: ${sum}`);
+//Problem no 03
+//var mainArray = []
+//for (var i = 0; i < 5; i++) {
+    //var userInput = +prompt("Please enter elements to the Array");
+    //mainArray.push(userInput);
+    //console.log(mainArray[i]);
+//}
+//let largerNum = mainArray[0];
+//for (let i = 0; i < mainArray.length; i++) {
+    //if (mainArray[i] > largerNum) {
+        //largerNum = mainArray[i];
+    //}
+//}
+//console.log(`The Largest Number in this Array is: ${largerNum}`)
+
+//Problem no 04
+    //var userInput = prompt("Please enter a string?")
+    //var reverse = "";
+    //for (let i = userInput.length - 1; i >= 0; i--){
+        //reverse += userInput[i];
+    //}
+    //console.log(`The Reverse String is: ${reverse}`)
+
+    //Problem no 05
+    var userInput = +prompt("Please enter a Number?")
+    var factorial = 1;
+    for (let i = 2; i <= userInput; i++) {
+        factorial *= i;
+    }
+    console.log(`The Factorial is:${factorial}`)
+
